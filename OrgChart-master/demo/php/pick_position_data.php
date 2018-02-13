dca71656429051322066bda37f18bd4f39d613fe
zhdllwyc <yingchew@usc.edu>
2018-02-12 20:16:57 -0800

    position employee relation

--- OrgChart-master/demo/php/pick_position_data.php
+++ OrgChart-master/demo/php/pick_position_data.php
@@ -7,38 +7,42 @@ Replace database_username and database_password
 with the SQL Server database username and password.
 */
 
-echo "hehe";
-$employee_id_initial=$_POST['employee_id'];
+
+$employee_id_initial=$_POST["employee_id"];
 $Servername='Assessor';
 $connection_info=array('UID'=>'zhdllwyc',
 	'PWD'=> '19960806Wyc',
 	'Database'=>'PositionControl',
 'ReturnDatesAsStrings'=>true);
-echo "ka";
+
 
 // Connect to the data source and get a handle for that connection.
 $conn=sqlsrv_connect($Servername,$connection_info);
 
-echo "emmm";
+
 if ($conn===false){
 	echo "unable to connect";
 	die(print_r(sqlsrv_errors(),true));
 
 }
-else{
- echo "connect";
-}
 
-$stmt_employee="SELECT * FROM dbo.position WHERE (HOME_UNIT_CD = (SELECT HOME_UNIT_CD from dbo.position WHERE POSN_ID = (SELECT POSN_ID from dbo.EMPLOYEE_POSITION WHERE( employee_id = 415748))))
+$stmt_employee="SELECT * FROM dbo.position WHERE (HOME_UNIT_CD = (SELECT HOME_UNIT_CD from dbo.position WHERE POSN_ID = (SELECT POSN_ID from dbo.EMPLOYEE_POSITION WHERE( EMPLOYEE_ID = $employee_id_initial))))
 ";
 
 $stmt = sqlsrv_query( $conn, $stmt_employee);
 if($stmt===false){
    echo "sbsbssbsbbsbs";
 }else{
+
 	$result=array();
 	while($row = sqlsrv_fetch_array($stmt)) {
-    	$result[] = $row["POSN_ID"].", ".$row["HOME_UNIT_CD"];
+		$myobject= new \stdClass();
+		$myobject->position_id=$row["POSN_ID"];
+		$myobject->home_unit_cd=$row["HOME_UNIT_CD"];
+
+		$myjson=json_encode($myobject);
+
+    	$result[] = $myjson;
 	}
 	echo json_encode($result);
 }
--- OrgChart-master/demo/php/pick_relation.php
+++ OrgChart-master/demo/php/pick_relation.php
@@ -7,38 +7,42 @@ Replace database_username and database_password
 with the SQL Server database username and password.
 */
 
-echo "hehe";
-$employee_id_initial=$_POST['employee_id'];
+
+$employee_id_initial=$_POST["employee_id"];
 $Servername='Assessor';
 $connection_info=array('UID'=>'zhdllwyc',
 	'PWD'=> '19960806Wyc',
 	'Database'=>'PositionControl',
 'ReturnDatesAsStrings'=>true);
-echo "ka";
+
 
 // Connect to the data source and get a handle for that connection.
 $conn=sqlsrv_connect($Servername,$connection_info);
 
-echo "emmm";
 if ($conn===false){
 	echo "unable to connect";
 	die(print_r(sqlsrv_errors(),true));
 
 }
-else{
- echo "connect";
-}
 
-$stmt_employee="SELECT * from dbo.EMPLOYEE_POSITION where (POSN_ID IN (select POSN_ID from dbo.position where HOME_UNIT_CD = (select HOME_UNIT_CD from dbo.position where( POSN_ID = (SELECT POSN_ID FROM dbo.EMPLOYEE_POSITION WHERE (employee_id = 415748)) ))))
+
+$stmt_employee="SELECT * from dbo.EMPLOYEE_POSITION where (POSN_ID IN (select POSN_ID from dbo.POSITION where HOME_UNIT_CD = (select HOME_UNIT_CD from dbo.POSITION where( POSN_ID = (SELECT POSN_ID FROM dbo.EMPLOYEE_POSITION WHERE (EMPLOYEE_ID = $employee_id_initial)) ))))
 ";
 
 $stmt = sqlsrv_query( $conn, $stmt_employee);
 if($stmt===false){
    echo "sbsbssbsbbsbs";
 }else{
+
 	$result=array();
 	while($row = sqlsrv_fetch_array($stmt)) {
-    	$result[] = $row["employee_id"].", ".$row["POSN_ID"];
+		$myobject= new \stdClass();
+		$myobject->position_id=$row["POSN_ID"];
+		$myobject->employee_id=$row["EMPLOYEE_ID"];
+
+		$myjson=json_encode($myobject);
+
+    	$result[] = $myjson;
 	}
 	echo json_encode($result);
 }
