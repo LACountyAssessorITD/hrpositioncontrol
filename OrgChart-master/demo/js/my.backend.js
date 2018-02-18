f25f37035dbb5ee2d334347a489a414225c71425
zhdllwyc <yingchew@usc.edu>
2018-02-17 16:17:50 -0800

    separate js files

---
+++ OrgChart-master/demo/js/my.backend.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+     var myData= {
+          'employee_id': '415748'
+        };
+
+     var datasource;
+
+    $.ajax({
+      url: "php/pick_position_data.php",
+      data: myData,
+      type: 'POST',
+      dataType: "json",
+      success: function(output) {
+        alert ('success: output=' + output);
+        runindex2(output);
+      },
+      error: function(xhr, status, error){
+        alert ('error: error=' + error + '; status=' + status);
+      },
+      async:false
+    });
+
+  function runindex2(position_data) {
+    console.log('runindex2');
+    $.ajax({
+     url: 'php/pick_data.php',
+     data: myData,
+     type: 'POST',
+     dataType: "json",
+     success: function(output) {
+        alert ('success: output=' + output);
+       runindex3(position_data,output);
+     },
+     error: function(xhr, status, error){
+      alert (error);
+      alert('status: ' + status);
+    },
+    async:false
+  });
+  }
+
+  function runindex3(position_data,employee_data) {
+    console.log('runindex3');
+    $.ajax({
+     url: 'php/pick_relation.php',
+     data: myData,
+     type: 'POST',
+     dataType: "json",
+     success: function(output) {
+
+      datasource=get_data(position_data,employee_data,output);
+      // console.log(datasource);
+
+    },
+    error: function(xhr, status, error){
+      alert (error);
+      alert('status: ' + status);
+    },
+    async:false
+  });
+  }
+
+  function get_data(position,employee, relation){
+     var head_id=myData['employee_id'];
+
+     var position_head_id=get_position(head_id, relation);
+     var head_unit_cd=get_home_unit_cd(head_id, employee);
+      var head_employee={'name':head_id, 'title': head_unit_cd,'position':position_head_id ,'children':[]};
+      var head_child=get_children(head_id, employee);
+      for (var i=0;i<head_child.length; i++){
+        var single_child=get_data_helper(head_child[i],position,employee, relation);
+          head_employee.children.push(single_child);
+      }
+      return head_employee;
+  }
+
+  function get_data_helper(employee_id,position,employee, relation){
+
+     var position_current_id=get_position(employee_id, relation);
+     var current_unit_cd=get_home_unit_cd(employee_id, employee);
+      var current_employee={'name':employee_id, 'title': current_unit_cd,'position':position_current_id };
+      var current_child=get_children(employee_id, employee);
+      if(current_child.length==0){
+        // console.log("00000");
+          return current_employee;
+      }else{
+        current_employee.children=[]
+      for (var i=0;i<current_child.length; i++){
+        var single_current_child=get_data_helper(current_child[i],position,employee, relation);
+        current_employee.children.push(single_current_child);
+      }
+      return current_employee;
+    }
+
+  }
+
+  function get_position(employee_id, relation){
+
+       for (var i=0; i<relation.length;i++){
+
+          if( relation[i]['employee_id'].toString().trim()==employee_id.toString().trim()){
+
+              return relation[i]['position_id'].toString().trim();
+      }
+    }
+  }
+
+  function get_home_unit_cd(employee_id, employee){
+       for (var i=0; i<employee.length;i++){
+
+          if(employee[i]['employee_id'].toString().trim()==employee_id.toString().trim()){
+              return employee[i]['home_unit_cd'].toString().trim();
+      }
+    }
+  }
+
+  function get_children(employee_id, employee){
+    var children=[];
+    // console.log("hhhh");
+    // console.log(employee_id);
+    for (var i=0; i<employee.length;i++){
+            if(employee[i]['supervisor_id'].toString().trim()==employee_id.toString().trim()){
+              if(employee[i]['employee_id'].toString().trim()!=415748){
+                children.push(employee[i]['employee_id'].toString().trim());
+                // console.log(employee[i]['employee_id'].toString().trim());
+              }
+
+        }
+      }
+      // console.log("kkkk");
+      return children;
+  }
+  myOrgchart(datasource);
+});
---
+++ OrgChart-master/demo/js/my.frontend.js
@@ -0,0 +1,207 @@
+function myOrgchart(datasource) {
+    // var datasource = {
+    //   'name': 'Lao Lao',
+    //   'title': 'general manager',
+    //   'position': 'position1',
+    //   'children': [
+    //     { 'name': '', 'title': '', 'position': 'position2' },
+    //     { 'name': 'Su Miao', 'title': 'department manager', 'position': 'position3',
+    //       'children': [
+    //         { 'name': 'Tie Hua', 'title': 'senior engineer', 'EmployeeId':'900','position': 'position4' },
+    //         { 'name': 'Hei Hei', 'title': 'senior engineer', 'position': 'position5' }
+    //       ]
+    //     },
+    //     { 'name': 'Yu Jie', 'title': 'department manager', 'position': 'position4' },
+    //     { 'name': 'Yu Li', 'title': 'department manager', 'position': 'position4' },
+    //     { 'name': 'Hong Miao', 'title': 'department manager', 'position': 'position4' },
+    //     { 'name': 'Yu Wei', 'title': 'department manager', 'position': 'position4' },
+    //     { 'name': 'Chun Miao', 'title': 'department manager', 'position': 'position4' },
+    //     { 'name': 'Yu Tie', 'title': 'department manager', 'position': 'position4' }
+    //   ]
+    // };
+
+    var getId = function() {
+      return (new Date().getTime()) * 1000 + Math.floor(Math.random() * 1001);
+    };
+
+    var nodeTemplate = function(data) {
+      return '<div class="position">' + data.position +
+          '<div class="employee" draggable="true"> <!--referenced as innerNode in .js file-->' +
+            '<div class="title">' + data.name + '</div>' +
+            '<div class="content">' + data.title + '</div>' +
+            '<span class="tooltiptext">' +
+             'Employee ID:  <span class="EmployeeId">' + data.name + '</span> <br>' +
+              'Position Status Code: ACCTG<br>' +
+              'Position Action Code: ORG<br>' +
+            '</span>' +
+          '</div>' +
+        '</div>';
+    };
+    // var nodeTemplate = function(data) {
+    //   return `
+    //     <div class="position">${data.position}
+    //       <div class="employee" draggable="true"> <!--referenced as innerNode in .js file-->
+    //         <div class="title">${data.name}</div>
+    //         <div class="content">${data.title}</div>
+    //         <span class="tooltiptext">
+    //          Employee ID:  <span class="EmployeeId"> ${data.EmployeeId} </span> <br>
+    //           Position Status Code: ACCTG<br>
+    //           Position Action Code: ORG<br>
+    //           Function Code: CD53
+    //         </span>
+    //       </div>
+    //     </div>
+    //   `;
+    // };
+
+    var oc = $('#chart-container').orgchart({
+      'data' : datasource,
+      'nodeContent': 'title',
+      'nodeTemplate': nodeTemplate,
+      'draggable': true,
+      'parentNodeSymbol': 'fa-th-large',
+      'chartClass': 'edit-state',
+      'createNode': function($node, data) {
+        $node[0].id = getId();
+      }
+    });
+
+    //edit chart script
+    oc.$chartContainer.on('click', '.node', function() {
+      var $this = $(this);
+      $('#selected-node').val($this.find('.title').text()).data('node', $this);
+    });
+
+    oc.$chartContainer.on('click', '.orgchart', function(event) {
+      if (!$(event.target).closest('.node').length) {
+        $('#selected-node').val('');
+      }
+    });
+
+    $('input[name="chart-state"]').on('click', function() {
+      $('.orgchart').toggleClass('edit-state', this.value !== 'view');
+      $('#edit-panel').toggleClass('edit-state', this.value === 'view');
+      if ($(this).val() === 'edit') {
+        $('.orgchart').find('tr').removeClass('hidden')
+          .find('td').removeClass('hidden')
+          .find('.node').removeClass('slide-up slide-down slide-right slide-left');
+      } else {
+        $('#btn-reset').trigger('click');
+      }
+    });
+
+    $('input[name="node-type"]').on('click', function() {
+      var $this = $(this);
+      if ($this.val() === 'parent') {
+        $('#edit-panel').addClass('edit-parent-node');
+        $('#new-nodelist').children(':gt(0)').remove();
+      } else {
+        $('#edit-panel').removeClass('edit-parent-node');
+      }
+    });
+
+    $('#btn-add-input').on('click', function() {
+      $('#new-nodelist').append('<li><input type="text" class="new-node"></li>');
+    });
+
+    $('#btn-remove-input').on('click', function() {
+      var inputs = $('#new-nodelist').children('li');
+      if (inputs.length > 1) {
+        inputs.last().remove();
+      }
+    });
+
+    $('#btn-add-nodes').on('click', function() {
+      var $chartContainer = $('#chart-container');
+      var nodeVals = [];
+      $('#new-nodelist').find('.new-node').each(function(index, item) {
+        var validVal = item.value.trim();
+        if (validVal.length) {
+          nodeVals.push(validVal);
+        }
+      });
+      var $node = $('#selected-node').data('node');
+      if (!nodeVals.length) {
+        alert('Please input value for new node');
+        return;
+      }
+      var nodeType = $('input[name="node-type"]:checked');
+      if (!nodeType.length) {
+        alert('Please select a node type');
+        return;
+      }
+      if (nodeType.val() !== 'parent' && !$('.orgchart').length) {
+        alert('Please creat the root node firstly when you want to build up the orgchart from the scratch');
+        return;
+      }
+      if (nodeType.val() !== 'parent' && !$node) {
+        alert('Please select one node in orgchart');
+        return;
+      }
+      if (nodeType.val() === 'parent') {
+        if (!$chartContainer.children('.orgchart').length) {// if the original chart has been deleted
+          oc = $chartContainer.orgchart({
+            'data' : { 'name': nodeVals[0] },
+            'exportButton': true,
+            'exportFilename': 'SportsChart',
+            'parentNodeSymbol': 'fa-th-large',
+            'createNode': function($node, data) {
+              $node[0].id = getId();
+            }
+          });
+          oc.$chart.addClass('view-state');
+        } else {
+          oc.addParent($chartContainer.find('.node:first'), { 'name': nodeVals[0], 'id': getId() });
+        }
+      } else if (nodeType.val() === 'siblings') {
+        if ($node[0].id === oc.$chart.find('.node:first')[0].id) {
+          alert('You are not allowed to directly add sibling nodes to root node');
+          return;
+        }
+        oc.addSiblings($node, nodeVals.map(function (item) {
+            return { 'name': item, 'relationship': '110', 'id': getId() };
+          }));
+      } else {
+        var hasChild = $node.parent().attr('colspan') > 0 ? true : false;
+        if (!hasChild) {
+          var rel = nodeVals.length > 1 ? '110' : '100';
+          oc.addChildren($node, nodeVals.map(function (item) {
+              return { 'name': item, 'relationship': rel, 'id': getId() };
+            }));
+        } else {
+          oc.addSiblings($node.closest('tr').siblings('.nodes').find('.node:first'), nodeVals.map(function (item) {
+              return { 'name': item, 'relationship': '110', 'id': getId() };
+            }));
+        }
+      }
+    });
+
+    $('#btn-delete-nodes').on('click', function() {
+      var $node = $('#selected-node').data('node');
+      if (!$node) {
+        alert('Please select one node in orgchart');
+        return;
+      } else if ($node[0] === $('.orgchart').find('.node:first')[0]) {
+        if (!window.confirm('Are you sure you want to delete the whole chart?')) {
+          return;
+        }
+      }
+      oc.removeNodes($node);
+      $('#selected-node').val('').data('node', null);
+    });
+
+    $('#btn-reset').on('click', function() {
+      $('.orgchart').find('.focused').removeClass('focused');
+      $('#selected-node').val('');
+      $('#new-nodelist').find('input:first').val('').parent().siblings().remove();
+      $('#node-type-panel').find('input').prop('checked', false);
+    });
+
+    //console output for drag and drop
+    oc.$chart.on('nodedrop.orgchart', function(event, extraParams) {
+      console.log('draggedNode:' + extraParams.draggedNode.children().children().children('.title').text()
+        + ', dragZone:' + extraParams.dragZone.children().children().children('.title').text()
+        + ', dropZone:' + extraParams.dropZone.children().children().children('.title').text()
+        );
+    });
+};
